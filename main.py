77fd66322f54ec2de0abf30fda9d714398ce2c9a
Bryantw12 <b.ashton522@gmail.com>
2023-05-04 10:07:32 -0400

    m

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect , send_from_directory , abort 
+from flask import Flask, render_template, request, redirect , send_from_directory , abort, g
 
 from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 
@@ -25,7 +25,8 @@ class User:
           return str(self.id)
 
 
-connection = pymysql.connect(
+def connect_db():
+    return pymysql.connect(
     host="10.100.33.60",
     user="bwilliams",
     password="244727137",
@@ -34,10 +35,22 @@ connection = pymysql.connect(
     autocommit=True
 )
 
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close()
+
 
 @login_manager.user_loader
 def user_loader(user_id):
-     cursor = connection.cursor()
+     cursor = cursor = get_db().cursor().cursor()
 
      cursor.execute("SELECT * from `users` WHERE `id` = " + user_id)
 
@@ -63,7 +76,7 @@ def index():
 @login_required
 def post_feed():
 
-    cursor = connection.cursor()
+    cursor = cursor = get_db().cursor().cursor()
 
     cursor.execute("SELECT * FROM  `posts` ORDER BY `TimeStamp`")
 
@@ -77,7 +90,7 @@ def post_feed():
 @app.route('/post', methods=['POST'])
 @login_required
 def create_post():
-     cursor = connection.cursor
+     cursor = cursor = get_db().cursor().cursor
 
      photo = request.files['photo']
 
@@ -110,7 +123,7 @@ def sign_in():
 
      if request.method == 'POST':
 
-          cursor = connection.cursor()
+          cursor = cursor = get_db().cursor().cursor()
 
           cursor.execute(f"SELECT * FROM `users` WHERE `username` =  '{request.form ['username']}' ")
 
@@ -143,7 +156,7 @@ def sign_up():
 
      if request.method =='POST':
        
-        cursor = connection.cursor()
+        cursor = cursor = get_db().cursor().cursor()
 
         photo = request.files ['photo']
 
@@ -171,7 +184,7 @@ def sign_up():
 @app.route('/profile/<username>')   
 
 def user_profile(username):
-    cursor = connection.cursor()
+    cursor = cursor = get_db().cursor().cursor()
 
     cursor.execute("SELECT * FROM `users` WHERE `username` = %s",(username))
 
@@ -182,7 +195,7 @@ def user_profile(username):
 
 
     cursor.close()
-    cursor = connection.cursor()
+    cursor = cursor = get_db().cursor().cursor()
     cursor.execute("SELECT * from `post` WHERE `user_id` = %s", (result['id']))
 
     post_result = cursor.fetchall()
